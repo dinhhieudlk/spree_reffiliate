ea2d72c7efc8c3f20d22b99c9234fe06caf4e6e5
Dinh Hieu <hieu.huynh@rubiik.com>
2017-08-06 18:06:49 +0700

    add affiliate to sidebarmenu

--- app/overrides/spree/admin/shared/_configuration_menu.rb
+++ app/overrides/spree/admin/shared/_configuration_menu.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :virtual_path => "spree/layouts/admin",
   :name => "admin_content_admin_configuration_menu_parser",
   :insert_bottom => "#main-sidebar",
-  :text => "<%= configurations_sidebar_menu_item Spree.t(:affiliates), admin_affiliates_path %>"
+  partial: "spree/admin/shared/_affiliates_sidebar_menu.html"
 )
