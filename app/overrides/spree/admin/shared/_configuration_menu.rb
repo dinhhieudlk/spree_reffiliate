fc51fad0d2a3feeaf6b9b85e393b81b7b3f726dd
Đình Hiếu <dinhhieu.dlk@gmail.com>
2017-08-06 18:10:36 +0700

    Update _configuration_menu.rb

--- app/overrides/spree/admin/shared/_configuration_menu.rb
+++ app/overrides/spree/admin/shared/_configuration_menu.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :virtual_path => "spree/layouts/admin",
   :name => "admin_content_admin_configuration_menu_parser",
   :insert_bottom => "#main-sidebar",
-  partial: "spree/admin/shared/_affiliates_sidebar_menu.html"
+  partial: "spree/admin/shared/_affiliates_sidebar_menu"
 )
