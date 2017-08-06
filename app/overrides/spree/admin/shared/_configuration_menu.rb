c9790bb60b1d2cb2c5f8c02173889c97f4438ebe
Đình Hiếu <dinhhieu.dlk@gmail.com>
2017-08-06 17:45:25 +0700

    Update _configuration_menu.rb

--- app/overrides/spree/admin/shared/_configuration_menu.rb
+++ app/overrides/spree/admin/shared/_configuration_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-  :virtual_path => "spree/admin/shared/_configuration_menu",
+  :virtual_path => "spree/layouts/admin",
   :name => "admin_content_admin_configuration_menu_parser",
-  :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+  :insert_bottom => "#main-sidebar",
   :text => "<%= configurations_sidebar_menu_item Spree.t(:affiliates), admin_affiliates_path %>"
 )
